180ffbb1ada89c021b9bdfc2f880eaa0ffab1cd3
swlee23 <swjj0320@naver.com>
2022-06-30 02:46:14 +0900

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DarkMode from './components/DarkMode';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <DarkMode></DarkMode>
     <App />
   </React.StrictMode>
 );
@@ -17,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//https://www.freecodecamp.org/news/portfolio-app-using-react-618814e35843/
